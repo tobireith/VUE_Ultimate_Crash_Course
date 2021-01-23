f23fde4dbb8ba6576b8a9c1be6740908444dbf8f
Tobias Reithmaier <tobias@reithmaier.de>
2021-01-23 13:32:09 +0100

    VUE Animations and 3rd Party Libraries
    Animations can be added by using the <transition> element.
    Therefore I've added some custom animations.
    
    You can also add 3rd party animations e.g. by adding the
    enter-active-class="animated flipInX" attribute to the transition
    element.
    Therefore I've used an external animation library.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,12 +3,18 @@
     <div class="holder">
       <form @submit.prevent="addSkill">
         <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
-        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
       </form>
+
       <ul>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
           <li v-for="(data, index) in skills" :key='index'> 
             {{ data.skill }} 
           </li>
+        </transition-group>
       </ul>
 
       <p> These are the skills that you possess.</p>
@@ -48,6 +54,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
   .holder {
     background: #fff;
   }
@@ -93,4 +102,25 @@ export default {
     padding: 5px;
     margin-top: -20px;
   }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
 </style>
