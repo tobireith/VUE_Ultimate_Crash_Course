7cca126aa703fcd979e5d4af0be6a9a8938d3104
Tobias Reithmaier <tobias@reithmaier.de>
2021-01-23 13:15:14 +0100

    Finished VUE Forms and Validators
    Added Input inside of a form.
    On Submit, the form adds a skill.
    Added a Validator for the input field from vee-validate. Validation
    result is shown in a paragraph.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import Vue from 'vue';
+import App from './App.vue';
+import VeeValidate from 'vee-validate';  // Add this
+Vue.use(VeeValidate);  // Add this
 Vue.config.productionTip = false
 
 new Vue({
